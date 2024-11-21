b79de2eedafe2a7833da323c2c15dcf6ba7f69ea
hxw05 <hxw05@whut.edu.cn>
2024-11-21 16:06:08 +0800

    部分通过，测试点4运行超时

---
+++ 20241121_PTA_数据结构实验3/t1修理牧场_sol1_AC.cpp
@@ -0,0 +1,56 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int n;
+    int a[9999];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int s = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex;
+        // 必须：minIndex指向的数组元素不能是空位（0）
+        for (int k = 0; k < n; k++) {
+            if (a[k] > 0) {
+                minIndex = k;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (a[j] == 0) continue;
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int subminIndex;
+        // 必须：满足不能是空位的同时，subminIndex和minIndex不能指向同一个元素。
+        for (int k = 0; k < n; k++)
+            if (a[k] > 0 && k != minIndex) {
+                subminIndex = k;
+                break;
+            }
+
+        for (int j = 0; j < n; j++) {
+            if (a[j] == 0) continue;
+            if (a[j] < a[subminIndex])
+                if ((a[j] == a[minIndex] && j != minIndex) || a[j] > a[minIndex]) {
+                    subminIndex = j;
+                }
+        }
+
+//        for (int j = 0; j < n; j++) {
+//            cout << a[j] << " ";
+//        }
+//
+//        cout << endl;
+
+        a[minIndex] = a[minIndex] + a[subminIndex];
+        s += a[minIndex];
+        a[subminIndex] = 0;
+    }
+
+    cout << s << endl;
+}
